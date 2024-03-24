877da8893bf2a800bd4c951541307209b827c505
Will Nayes <wnayes@gmail.com>
2024-03-23 20:52:59 -0500

    Use --first-parent

--- src/main/main.ts
+++ src/main/main.ts
@@ -33,7 +33,13 @@ function setup() {
   ipcMain.on("ready", () => {
     mainWindow.webContents.send("setBranch", branch);
 
-    const revlist = spawn("git", ["-C", repoPath, "rev-list", branch]);
+    const revlist = spawn("git", [
+      "-C",
+      repoPath,
+      "rev-list",
+      "--first-parent",
+      branch,
+    ]);
 
     let sentSome = false;
 
@@ -127,19 +133,8 @@ function setup() {
 app.whenReady().then(() => {
   setup();
   createWindow();
-
-  app.on("activate", function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
-    }
-  });
 });
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on("window-all-closed", function () {
-  if (process.platform !== "darwin") app.quit();
+app.on("window-all-closed", () => {
+  app.quit();
 });
